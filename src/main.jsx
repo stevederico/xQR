9873197b54e44a3ae029189137c16823d986c481
Steve Derico <steve@bixbyapps.com>
2026-03-28 08:59:38 -0700

    move analytics to dynamic script injection, add dockerignore

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeView from './components/HomeView.jsx';
 
+const s = document.createElement('script');
+s.defer = true;
+s.src = 'https://aob.bixbyapps.com/script.js';
+s.dataset.websiteId = '535354f8-732e-4630-b37a-b837ca1db1ba';
+s.dataset.domains = 'xqr.bixbyapps.com';
+document.head.appendChild(s);
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
