52428a1fda956d262e6deda0fe82c4201aeae3f6
Steve Derico <steve@bixbyapps.com>
2026-04-06 00:04:00 -0700

    1.7.0 add /:username route for direct profile links

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+1.7.0
+
+  Add username route
+
 1.6.0
 
   Fix XDK camelCase mapping
--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path="/" element={<HomeView />} />
       <Route path="/app/home" element={<HomeView />} />
+      <Route path="/:username" element={<HomeView />} />
       <Route path="*" element={<HomeView />} />
     </Routes>
   </BrowserRouter>
